9d21f622d49fc5582b69d3029df83874e3d28de2
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2015-09-10 23:24:53 -0600

    I am bad at mounting ISOs and should feel bad

--- common/virtualbox-guest-additions.sh
+++ common/virtualbox-guest-additions.sh
@@ -6,7 +6,7 @@ yum -y install dkms
 mountpoint='~/virtualbox-guest-addtions'
 guest_additions_iso='~/VBoxGuestAdditions.iso'
 mkdir ${mountpoint}
-mount -o loop ${mountpoint} ${guest_additions_iso}}
+mount -o loop ${guest_additions_iso} ${mountpoint}
 # The VirtualBox Guest Additions will return a failure exit code if X11 is not
 # present, so we have to override the return value
 /bin/sh ${mountpoint}/VBoxLinuxAdditions.run || true
