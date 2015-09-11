a48738f63162d07c4ef85b96a791e9c7bbb522de
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2015-09-10 23:46:25 -0600

    Create Guest Additions related files in /tmp

--- common/virtualbox-guest-additions.sh
+++ common/virtualbox-guest-additions.sh
@@ -3,8 +3,8 @@
 set -e
 
 yum -y install dkms
-mountpoint='~/virtualbox-guest-addtions'
-guest_additions_iso='~/VBoxGuestAdditions.iso'
+mountpoint=/tmp/virtualbox-guest-addtions
+guest_additions_iso=/tmp/VBoxGuestAdditions.iso
 mkdir ${mountpoint}
 mount -o loop ${guest_additions_iso} ${mountpoint}
 # The VirtualBox Guest Additions will return a failure exit code if X11 is not
