c6538c12cca1c4aa8561cba7c7b2de427611c300
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2015-09-12 15:56:18 -0600

    Best practice: always wrap bash variables in quotes

--- common/virtualbox-guest-additions.sh
+++ common/virtualbox-guest-additions.sh
@@ -8,18 +8,18 @@ yum -y install dkms bzip2
 # Mount the ISO
 mountpoint=/tmp/virtualbox-guest-addtions
 guest_additions_iso=/tmp/VBoxGuestAdditions.iso
-mkdir ${mountpoint}
-mount -o loop ${guest_additions_iso} ${mountpoint}
+mkdir "${mountpoint}"
+mount -o loop "${guest_additions_iso}" "${mountpoint}"
 
 # Install the VirtualBox Guest Additions
 # The installer will return a failure exit code if X11 is not present, so we 
 # have to override the return value
-/bin/sh ${mountpoint}/VBoxLinuxAdditions.run || true
+/bin/sh "${mountpoint}"/VBoxLinuxAdditions.run || true
 
 # Unmount the ISO
-umount ${mountpoint}
+umount "${mountpoint}"
 
 # Clean up
-rm -rf ${mountpoint}
-rm -rf ${guest_additions_iso}
+rm -rf "${mountpoint}"
+rm -rf "${guest_additions_iso}"
 
