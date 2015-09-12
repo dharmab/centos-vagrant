cffc1472bf19ccb524cf2a433d82cf8a8de30e95
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2015-09-12 15:54:28 -0600

    Document the shit outta the install scripts

--- common/virtualbox-guest-additions.sh
+++ common/virtualbox-guest-additions.sh
@@ -2,15 +2,24 @@
 
 set -e
 
+# Install dependencies
 yum -y install dkms bzip2
+
+# Mount the ISO
 mountpoint=/tmp/virtualbox-guest-addtions
 guest_additions_iso=/tmp/VBoxGuestAdditions.iso
 mkdir ${mountpoint}
 mount -o loop ${guest_additions_iso} ${mountpoint}
-# The VirtualBox Guest Additions will return a failure exit code if X11 is not
-# present, so we have to override the return value
+
+# Install the VirtualBox Guest Additions
+# The installer will return a failure exit code if X11 is not present, so we 
+# have to override the return value
 /bin/sh ${mountpoint}/VBoxLinuxAdditions.run || true
+
+# Unmount the ISO
 umount ${mountpoint}
+
+# Clean up
 rm -rf ${mountpoint}
 rm -rf ${guest_additions_iso}
 
